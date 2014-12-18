f9cbda865880c48f16b6af58274cd47140bc49ef
Gustavo Canales <guscanales@santanajorge.ryq.local>
2014-12-18 15:07:34 -0300

    Initial Commit

---
+++ CalculoNotas/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  CalculoNotas
+//
+//  Created by Gustavo Canales on 18-12-14.
+//  Copyright (c) 2014 GusCanales. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
